83ad31baa0a91abd282feb9c03849dc4f131cfc1
Anas AMIRI <anas.amiri@soprabanking.com>
2024-03-11 15:40:20 +0100

    Feat : rédaction de la class PenduModel avec la lecture d'un mot aléatoire, déplacement du fichier mots.txt

--- src/fr/ecole3il/rodez2023/pendu/model/PenduModel.java
+++ src/fr/ecole3il/rodez2023/pendu/model/PenduModel.java
@@ -1,4 +1,34 @@
 package fr.ecole3il.rodez2023.pendu.model;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
 public class PenduModel {
+
+    Mot mot;
+    public String fichier = System.getProperty("user.dir") + "/data/mots.txt";
+
+    public void genererMot(){
+        try {
+            Path path = Paths.get(fichier);
+            List<String> lines = Files.readAllLines(path);
+            int randomLineIndex = new Random().nextInt(lines.size());
+            String line = lines.get(randomLineIndex);
+            String[] parts = line.split(" ", 2);
+            String word = parts[0];
+            String definition = parts.length > 1 ? parts[1] : "";
+            mot = new Mot(word, definition);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            e.printStackTrace();
+        }
+    }
+
+    public Mot getMot(){
+        return mot;
+    }
 }
